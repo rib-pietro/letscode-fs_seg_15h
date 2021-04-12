7d10c78026f80635c7625886866f7886c646e4a6
pietro <Pietro Ribeiro>
2021-04-12 15:45:57 -0300

    aula 12-04-21 pre exercicios

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,56 @@
 from bottle import *
-# from bottle import run, route
-# import bottle
-# import bottle as bt
+
+posts = [
+    {
+        'url': 'beneficios-do-sono',
+        'titulo': 'Benefícios do sono',
+        'conteudo': 'Sono faz seu corpo relaxar, você consegue pensar mais facilmente nas dúvidas.'
+    },
+    {
+        'url': 'netflix',
+        'titulo': 'Netflix',
+        'conteudo': 'Netflix é bom!!!'
+    },
+    {
+        'url': 'passaros',
+        'titulo': 'Pássaros',
+        'conteudo': 'Pássaros são ajudados pelo Luis, voam e fazem cocô na nossa cabeça'
+    }
+]
 
 @route('/')
 def index():
-    return f"<h1>Hello, World!</h1>"
+
+    links_html = ''
+
+    for post in posts:
+        links_html += f'''
+        <li>
+            <a href="http://localhost:8080/posts/{url_post}">{post_title}</a>
+        </li>
+        '''
+
+    return f"""<h1>Blog da turma de Full Stack!</h1>
+    <h2> Posts: </h2>
+    <ul>
+        {links_html}
+    </ul>
+    """
 
 # rotas dinâmicas
-@route('/<nome>/<saudacao>')
-def hello(nome, saudacao):
-    return f"<h1>{saudacao}, {nome}!"
+
+@route('/posts/<url_post>')
+def get_post(url_post):
+    post_encontrado = None
+
+    for post in posts:
+        if post['url'] == url_post:
+            post_encontrado = post
+    
+    return f"""
+    <h1>{post_encontrado['titulo']}</h1>
+
+    <p>{post_encontrado['conteudo']}</p>
+    """
 
 run()
